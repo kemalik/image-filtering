0d91531ca846d2e6c861f9dbd103089f2a543e76
kema <kekenov.malik@gmail.com>
2017-07-22 12:59:22 +0600

    rename settings dir

--- server/project/settings.py
+++ server/project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'image_filtering.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'image_filtering.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
