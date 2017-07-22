aea6a296023850632e43b6b58d2256ebb9decfe4
kema <kekenov.malik@gmail.com>
2017-07-22 18:37:02 +0600

    add images model

--- server/applications/images/models.py
+++ server/applications/images/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.FileField('Image', upload_to='images')
+    creation_date = models.DateTimeField('Creation date', auto_now_add=True)
+
+    def __str__(self):
+        return self.id
--- server/project/settings.py
+++ server/project/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
-    'rest_framework'
+    'applications.api.apps.ApiConfig',
+    'rest_framework',
+    'applications.images.apps.ImagesConfig'
 ]
 
 MIDDLEWARE = [
