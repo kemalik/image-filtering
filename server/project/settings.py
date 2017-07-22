423ef5b3ace7294e8e56ce6120f44b000dc77ed9
kema <kekenov.malik@gmail.com>
2017-07-23 00:01:07 +0600

    remove api app

--- server/project/settings.py
+++ server/project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'applications.api.apps.ApiConfig',
     'rest_framework',
     'applications.images.apps.ImagesConfig'
 ]
