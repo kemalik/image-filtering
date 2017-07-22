968490b2d8066b1b940d4f5a7a9c5281960dc427
kema <kekenov.malik@gmail.com>
2017-07-23 00:07:16 +0600

    change image storage folder naming

--- server/applications/images/models.py
+++ server/applications/images/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Image(models.Model):
-    image = models.FileField('Image', upload_to='images')
+    image = models.FileField('Image', upload_to='images/%Y/%m/%d')
     creation_date = models.DateTimeField('Creation date', auto_now_add=True)
 
     def __str__(self):
