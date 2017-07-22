4cb1e4865780fe8ca255c7a13024ff2d541e0446
kema <kekenov.malik@gmail.com>
2017-07-23 00:00:44 +0600

    add api for images, add media root

---
+++ server/applications/images/api/viewsets.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from .serializers import ImageSerializer
+from applications.images.models import Image
+
+
+class ImagesViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows images to be viewed or edited.
+    """
+    queryset = Image.objects.all().order_by('-creation_date')
+    serializer_class = ImageSerializer
--- server/project/urls.py
+++ server/project/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from applications.images import urls as image_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/images/', include(image_urls, namespace='images'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
