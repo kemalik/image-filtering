bed3dd870c552a46869d5365f9c71145fa10ddfe
kema <kekenov.malik@gmail.com>
2017-07-23 15:22:01 +0600

    add filters app

--- server/project/settings.py
+++ server/project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'applications.images.apps.ImagesConfig'
+    'applications.images.apps.ImagesConfig',
+    'applications.filters.apps.FiltersConfig',
 ]
 
 MIDDLEWARE = [
