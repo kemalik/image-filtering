88861e1b3c9171b39fab200aeaf51e76679c8133
kema <kekenov.malik@gmail.com>
2017-07-25 12:48:53 +0600

    add checker for workers launched

--- server/applications/filters/tasks.py
+++ server/applications/filters/tasks.py
@@ -1,11 +1,20 @@
 from project.celery import app
 from celery.task.control import inspect
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
+def is_workers_launched():
+    insp = inspect()
+    if not insp.stats():
+        raise Exception('Workers not launched')
 
 
 def apply_filter(resource_id, image_id, filter_type):
-    insp = inspect()
-    d = insp.stats()
-    if d:
-        app.send_task('apply_filter', args=[resource_id, image_id, filter_type])
-        return True
-    return False
+    is_workers_launched()
+    app.send_task('apply_filter', args=[resource_id, image_id, filter_type])
+    logger.debug('Task send')
+
+
+is_workers_launched()
