9b21a8ea42cb50cd40b9ab7afdacf9ff9a23efa9
kema <kekenov.malik@gmail.com>
2017-07-25 13:47:34 +0600

    refactor base64 utility

---
+++ worker/helpers/base64_utils.py
@@ -0,0 +1,19 @@
+import base64
+
+
+class Base64Util(object):
+    def __init__(self, base_64_text) -> None:
+        self.base_64_text = base_64_text
+
+    def clear_base64(self, base64_img):
+        return base64_img.split('base64,')[-1].replace(' ', '+')
+
+    def convert_image_to_base64(self, file_path):
+        with open(file_path, 'rb') as f:
+            code = base64.b64encode(f.read())
+            return 'data:image/png;base64,{code}'.format(code=code.decode('utf8'))
+
+    def decode_base64(self):
+        cleaned_base64_image = self.clear_base64(self.base_64_text)
+
+        return base64.b64decode(cleaned_base64_image)
--- worker/tasks.py
+++ worker/tasks.py
@@ -5,9 +5,9 @@ from tempfile import NamedTemporaryFile
 
 from celery import Celery
 
-from api_client import ApiClient
-from base64_utils import clear_base64, convert_to_base64
-from filters import FilterApplier
+from helpers.api_client import ApiClient
+from helpers.base64_utils import Base64Util
+from helpers.filters import FilterApplier
 
 logger = getLogger(__name__)
 
@@ -23,24 +23,26 @@ def apply_filter(resource_id, image_id, filter_type):
     logger.info('Got resource_id={resource_id}, image_id{image_id}, filter_type={filter_type}'.format(
         resource_id=resource_id, image_id=image_id, filter_type=filter_type
     ))
+
     api_client = ApiClient()
     temp_file = NamedTemporaryFile(delete=False, suffix='.png', mode='wb')
 
     base64_image = api_client.get_image_source(image_id)
-    cleaned_base64_image = clear_base64(base64_image)
 
-    png_source = base64.b64decode(cleaned_base64_image)
+    base64_util = Base64Util(base64_image)
+    png_source = base64_util.decode_base64()
+
     temp_file.write(png_source)
 
     filter_applier = FilterApplier(temp_file.name, filter_type)
     filtered_file_name = filter_applier.apply_filter()
 
-    logger.info('Saved filtered_file_name={filtered_file_name}'.format(
-        filtered_file_name=filtered_file_name
-    ))
-
     if filtered_file_name:
-        filtered_base64_image = convert_to_base64(filtered_file_name)
+        logger.info('Saved filtered_file_name={filtered_file_name}'.format(
+            filtered_file_name=filtered_file_name
+        ))
+
+        filtered_base64_image = base64_util.convert_image_to_base64(filtered_file_name)
 
         filtered_image_id = api_client.create_image(filtered_base64_image)
 
